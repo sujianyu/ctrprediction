04da47bc81f6eff7f65c65ec5e9656cbe63e8b54
sujianyu <230677mbh>
2018-10-14 15:59:55 +0800

    CRT feature

--- ctr_feature.py
+++ ctr_feature.py
@@ -13,4 +13,10 @@ result = df[["C14","C15","C16","C17","C18","C19","C20","C21"]].describe()
 with open("train_sample_describe.csv","w") as f:
     pd.SparseDataFrame(result).to_csv(f)
 
-print(df.info())
+print(df["click"].mean())
+
+group = df["click"].groupby(df["device_type"])
+print(group.mean())
+
+print(df["C15"].value_counts())
+print(df["C16"].value_counts())
