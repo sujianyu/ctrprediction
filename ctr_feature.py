e25744f6c63b486d40e1aa882c2d02e9b81a02ad
sujianyu <sujianyu@foxmail.com>
2018-10-15 16:45:19 +0800

    new version

--- ctr_feature.py
+++ ctr_feature.py
@@ -4,16 +4,26 @@
 import numpy as np
 import pandas as pd
 import os
+import datetime
 import seaborn as sns
 import matplotlib.pyplot as plt
 datapath = "."
 trainfile = os.path.join(datapath ,"train_sample.csv")
 
 df = pd.read_csv(trainfile)
+'''
 result = df[["C14","C15","C16","C17","C18","C19","C20","C21"]].describe()
 
 with open("train_sample_describe.csv","w") as f:
     pd.SparseDataFrame(result).to_csv(f)
+'''
+def getweekday(date):
+    '''
+    :param date: YYMMDD
+    :return: weekday
+    '''
+    year = str(date)[0:]
+df.info()
 
 print(df["click"].mean())
 print("################")
@@ -52,4 +62,14 @@ print(df["C19"].value_counts())
 print(df["C20"].value_counts())
 print(df["C21"].value_counts())
 print("################")
-x_train = df.drop(["id","click"],axis = 1)
+y_train = df["click"]
+df = df.drop(["id","click"],axis=1)
+df["C15"] = df["C15"].map(lambda x:str(x))
+df["C16"] = df["C16"].map(lambda x:str(x))
+#建立新的特征列
+df["size"] = df["C15"].str.cat(df["C16"],sep="_")
+#将hour列拆分为
+df["hour1"] = df["hour"].map(lambda x:str(x)[6:8])
+df["day"] = df["hour"].map(lambda x:str(x)[4:6])
+
+print(df.head())
