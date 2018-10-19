2f5ef966f300408f43b9979c1e7814a369323a27
sujianyu <sujianyu@foxmail.com>
2018-10-19 16:59:03 +0800

    create new train dataset

--- gbdt_train.py
+++ gbdt_train.py
@@ -7,7 +7,10 @@ import sys,os
 import xgboost as xgb
 import datetime
 from sklearn import preprocessing
-
+from xgboost import plot_importance
+from matplotlib import pyplot as plt
+# 计算分类正确率
+from sklearn.metrics import accuracy_score
 def getweekday(x):
     '''
     :param x: YYMMDD
@@ -22,7 +25,7 @@ def getweekday(x):
     return weekday
 
 datapath = "."
-trainfile = os.path.join(datapath ,"train_sample2.csv")
+trainfile = os.path.join(datapath ,"train_sample.csv")
 df = pd.read_csv(trainfile,dtype={"C15":str,"C16":str})
 df["size"] = df["C15"].str.cat(df["C16"], sep="_")
 # 将hour列拆分为
@@ -34,20 +37,26 @@ df = df.drop(["id", "hour", "C15", "C16"], axis=1)
 # specify parameters via map
 param = {'max_depth':15, 'eta':.02, 'objective':'binary:logistic', 'verbose':0,
          'subsample':1.0, 'min_child_weight':50, 'gamma':0,
-         'nthread': 16, 'colsample_bytree':.5, 'base_score':0.16, 'seed': 999}
+         'nthread': 4, 'colsample_bytree':.5, 'base_score':0.16, 'seed': 999}
 num_round = 4
 y_train = df["click"]
 df = df.drop(["click"],axis=1)
-#print(df.head())
-df["C1"] = df["C1"].astype("category")
-print("C1")
-print(df["C1"])
 
-print(y_train)
 le = preprocessing.LabelEncoder()
-
-X_train = le.fit_transform(df.values)
-print(X_train.head())
-dtrain = xgb.DMatrix(X_train,label = y_train)
+columns = ["C1","site_id","site_domain","site_category","app_id","app_domain","app_category","device_id","device_ip","device_model","size"]
+for columnname in columns:
+    df[columnname]= le.fit_transform(df[columnname])
+X_train = df.values
+print(X_train)
+dtrain = xgb.DMatrix(X_train,y_train)
 
 bst = xgb.train(param, dtrain, num_round)
+train_preds = bst.predict(dtrain)
+train_predictions = [round(value) for value in train_preds]
+y_train = dtrain.get_label()
+train_accuracy = accuracy_score(y_train, train_predictions)
+print ("Train Accuary: %.2f%%" % (train_accuracy * 100.0))
+
+# 显示重要特征
+plot_importance(bst)
+plt.show()
