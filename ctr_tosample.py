42bd38d450357f120482b20aadce69610ccaa381
sujianyu <sujianyu@foxmail.com>
2018-10-23 17:14:55 +0800

    create new train dataset

--- ctr_tosample.py
+++ ctr_tosample.py
@@ -7,7 +7,7 @@ import sys,os
 datapath = "."
 #print(datapath)
 trainfile = os.path.join(datapath ,"train.csv")
-
+testfile = os.path.join(datapath,"test")
 df = pd.read_csv(trainfile,chunksize=100000)
 #数据随机采样
 '''
@@ -23,10 +23,23 @@ with open("train_sample2.csv","a") as samplefile:
             sample.to_csv(samplefile, index=False, header=True)
         else:
             sample.to_csv(samplefile, index=False, header=False)
-        index = index + 1
+        index +=1
         print(index)
         #print(chunk.columns)
 
         count = count + chunk.shape[0]
-
-print(count)
+    print(count)
+#测试数据生成微小数据集
+df_test = pd.read_csv(testfile,chunksize=100000)
+print("生成测试数据微小数据集")
+index = 0
+with open("test_sample","a") as testsamplefile:
+    for chunk in df_test:
+        sample_test = chunk.sample(n=None,frac=0.1,axis=0)
+        sample_test["id"] = sample_test["id"].apply(lambda x: '{:.0f}'.format(x))
+        if index ==0:
+            sample_test.to_csv(testsamplefile,index=False,header=True)
+        else:
+            sample_test.to_csv(testsamplefile, index=False, header=False)
+        index +=1
+    print("测试集完成。")
--- gbdt_LR_train.py
+++ gbdt_LR_train.py
@@ -10,8 +10,11 @@ import datetime
 from  sklearn import metrics
 from sklearn import preprocessing
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import log_loss
 from xgboost import plot_importance
 from matplotlib import pyplot as plt
+import joblib
 # 计算分类正确率
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
@@ -28,67 +31,110 @@ def getweekday(x):
     weekday = dt.weekday()
     return weekday
 
+def create_feature(data):
+    data["size"] = data["C15"].str.cat(data["C16"], sep="_")
+    # 将hour列拆分为
+    data["hour1"] = data["hour"].map(lambda x: str(x)[6:8])
+    data["day"] = data["hour"].map(lambda x: str(x)[4:6])
+    data["weekday"] = data["hour"].map(lambda x: getweekday(x))
+    data["app_site_id"] = data["app_id"] + "_" + data["site_id"]
+    data["app_site_id_model"] = data["app_site_id"] + "_" + data["device_model"]
+    #此处可以考虑将组合特征的源特征删掉，对比效果
+    data = data.drop(["id", "hour", "C15", "C16"], axis=1)
+    return data
+
 datapath = "."
 trainfile = os.path.join(datapath ,"train_sample2.csv")
-df = pd.read_csv(trainfile,dtype={"C15":str,"C16":str})
-df["size"] = df["C15"].str.cat(df["C16"], sep="_")
-# 将hour列拆分为
-df["hour1"] = df["hour"].map(lambda x: str(x)[6:8])
-df["day"] = df["hour"].map(lambda x: str(x)[4:6])
-df["weekday"] = df["hour"].map(lambda x: getweekday(x))
-df = df.drop(["id", "hour", "C15", "C16"], axis=1)
 
+df_train = pd.read_csv(trainfile,dtype={"C15":str,"C16":str})
+#df_test= pd.read_csv(testfile,dtype={"C15":str,"C16":str})
+df_train = create_feature(df_train)
+#df_test = create_feature(df_test)
+#删掉组合特征的原始特征，为了省内存
+df_train = df_train.drop(["app_id","site_id","device_model"],axis=1)
 # specify parameters via map
 param = {'max_depth':15, 'eta':.02, 'objective':'binary:logistic', 'verbose':0,
          'subsample':1.0, 'min_child_weight':50, 'gamma':0,
          'nthread': 4, 'colsample_bytree':.5, 'base_score':0.16, 'seed': 999}
 num_round = 5
-y_train = df["click"]
-df = df.drop(["click"],axis=1)
+y_all = df_train["click"]
+df_train = df_train.drop(["click"],axis=1)
 
 le = preprocessing.LabelEncoder()
-columns = ["C1","site_id","site_domain","site_category","app_id","app_domain","app_category","device_id","device_ip","device_model","size"]
-for columnname in columns:
-    df[columnname]= le.fit_transform(df[columnname])
-X_train = df.values
-print(X_train)
-dtrain = xgb.DMatrix(X_train,y_train)
+columns_id = ["site_domain","site_category","app_domain","app_category","device_id","device_ip","app_site_id","app_site_id_model","size"]
 
-bst = xgb.train(param, dtrain, num_round)
+for columnname in columns_id:
+    df_train[columnname]= le.fit_transform(df_train[columnname])
+
+X_all = df_train.values
+print(X_all.shape)
+#划分数据集
+print("划分数据集")
+#x_train, x_val, y_train, y_val = train_test_split(X_all, y_all, test_size = 0.2, random_state = 2018)
+
+dtrain = xgb.DMatrix(X_all,y_all)
+#dtest = xgb.DMatrix(x_val,y_val)
+bst = xgb.train(param, dtrain, num_round,early_stopping_rounds=10)
 #pred_leaf=True,
-train_preds = bst.predict(dtrain,pred_leaf=False)
-print(train_preds.shape)
+'''
 
+test_preds = bst.predict(dtest,pred_leaf=False)
+print(test_preds.shape)
 
-train_predictions = [round(value) for value in train_preds]
-y_train = dtrain.get_label()
-train_accuracy = accuracy_score(y_train, train_predictions)
+test_predictions = [round(value) for value in test_preds]
+y_test = dtest.get_label()
+train_accuracy = accuracy_score(y_test, test_predictions)
 print ("Train Accuary: %.2f%%" % (train_accuracy * 100.0))
+'''
 # 显示重要特征
 plot_importance(bst)
-plt.show()
+plt.savefig("feature_importtance.png")
+#保存xgboost模型数据
+print("saving xgboost model")
+joblib.dump(bst,"xgb_ctr_joblib.dat")
+
+#plt.show()
 #得到新特征
 x_train_feature = bst.predict(dtrain,pred_leaf=True)
-new_feature = DataFrame(x_train_feature)
-print(new_feature.shape)
-print(new_feature.head())
+#x_test_feature = bst.predict(dtest,pred_leaf=True)
 
+new_train_feature = DataFrame(x_train_feature)
+#new_test_feature = DataFrame(x_test_feature)
+print(new_train_feature.shape)
+print(new_train_feature.head())
 #对新特征使用onehot编码
 enc = OneHotEncoder()
-new_feature_onehot = enc.fit_transform(new_feature).toarray()
-print(new_feature_onehot.shape)
-
+new_train_feature_onehot = enc.fit_transform(new_train_feature).toarray()
+#new_test_feature_onehot = enc.fit_transform(new_test_feature).toarray()
+print(new_train_feature_onehot.shape)
+#GBDT生成的特征划分数据集
+x_train_lr, x_val_lr, y_train_lr, y_val_lr = train_test_split(new_train_feature_onehot, y_all, test_size=0.2, random_state=2018)
 #进行LR预测
 # 定义LR模型
 lr = LogisticRegression()
-lr.fit(new_feature_onehot,y_train)
-y_pred_xgblr1_proba = lr.predict_proba(new_feature_onehot)
-y_pred_xgblr = lr.predict(new_feature_onehot)
+lr.fit(x_train_lr,y_train_lr)
+
+y_pred_xgblr1_proba = lr.predict_proba(x_train_lr)
+y_pred_xgblr = lr.predict(x_train_lr)
+tr_logloss = log_loss(y_train_lr,y_pred_xgblr1_proba[:,1])
+print("train logloss:",tr_logloss)
+val_logloss = log_loss(y_val_lr,lr.predict_proba(x_val_lr)[:,1])
+print("validate logloss:",val_logloss)
 #print(y_pred_xgblr)
-total = len(y_train)
-correct = 0
-for index in range(total):
-    if y_train[index] == y_pred_xgblr[index]:
-        correct += 1
-#print(y_pred_xgblr1_proba)
-print("XGB _+ LR :",total,correct,correct * 1.0 /total)
+#保存logRegression
+print("saving LogRegression Model data")
+joblib.dump(lr,"ctr_lr_joblib.dat")
+#开始预测
+print("用测试集数据进行预测。")
+testfile = os.path.join(datapath,"test_sample")
+df_test = pd.read_csv(testfile)
+
+#写入结果文件中
+print("Write result to test file.")
+res = pd.read_csv("test_sample")
+y_pred  = [0]
+res["click"] = y_pred
+outputfilename = os.path.join(datapath,"test_predict.csv")
+with open(outputfilename) as outputfile:
+    res["id"] = res["id"].apply(lambda x: '{:.0f}'.format(x))
+    res.to_csv(outputfile,index=False,header=True)
